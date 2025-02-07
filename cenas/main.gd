65dc58f49126225fbfd9e5c83b9e823faa5541ff
amaiNikaido <149743846+amaiNikaido@users.noreply.github.com>
2025-02-07 16:47:52 -0300

    Mudei a fonte

--- cenas/main.gd
+++ cenas/main.gd
@@ -1,12 +1,19 @@
 extends Node2D
 
+var score := [0, 0]
 
-# Called when the node enters the scene tree for the first time.
+
+# Called when the node enters tdhe scene tree for the first time.
 func _ready() -> void:
 	start_game()
 
 func start_game() -> void:
-	$Raquete.position.x = 100
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	$Raquete.position.x = 16
+#	$Raquete2.position.x = 240
+	$Raquete.position.y = 72
+#	$Raquete2.position.y = 72
+	
+	
+	
 func _process(delta: float) -> void:
 	pass
