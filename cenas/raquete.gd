cc3923b9ab9ea53657420349082c21f56094bb25
NicolasFlor <mickflor9@gmail.com>
2025-02-07 17:08:39 -0300

    inicio de controles

--- cenas/raquete.gd
+++ cenas/raquete.gd
@@ -3,6 +3,7 @@ extends StaticBody2D
 var win_height : int
 var p_height : int
 var velocidade : int = 100
+var direcao : int 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,4 +13,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+
+	# Controles
+	if Input.is_action_pressed("Cima"):
+		
 	
