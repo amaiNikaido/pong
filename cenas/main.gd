44599ea66af079e4fd8e25a106cd563a52b34dc1
NicolasFlor <mickflor9@gmail.com>
2025-02-09 19:53:00 -0300

    Adicionei os popups e ajustei algumas coisas estéticas

--- cenas/main.gd
+++ cenas/main.gd
@@ -21,6 +21,9 @@ func start_game() -> void:
 func _process(delta: float) -> void:
 	if score[0] == 3 or score[1] == 3:
 		$Fim.show()
+		#Reinicia o jogo
+		if Input.is_action_just_pressed("enter"):
+			get_tree().reload_current_scene()
 
 
 func _on_timer_da_bola_timeout() -> void:
@@ -38,3 +41,7 @@ func _on_gol_esquerdo_body_entered(body):
 	$Hud/PontosJogador.text = str(score[0])
 	if score[0] < 3:
 		$TimerDaBola.start()
+
+func P_PowerUp() -> void:
+	if score[0] == 1 or score[1] == 1:
+		$PowerUpP.show()
--- cenas/raquete.gd
+++ cenas/raquete.gd
@@ -10,12 +10,29 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("Cima"):
-		position.y -= velocidade * delta
-	elif Input.is_action_pressed("Baixo"):
-		position.y += velocidade * delta
+func _process(_delta: float) -> void:
 	
+	$Label.text = str(Global.cor_raquete)
+	checa_botao_press(_delta)
+	if Global.cor_raquete == Global.cor_bola:
+		collision_mask != ~(1 << 1)
+	elif Global.cor_raquete != Global.cor_bola:
+		collision_mask &= ~(1 << 1)
+
+func _physics_process(delta: float) -> void:
+	velocity = velocity.normalized() * velocidade
+	move_and_collide(velocity * delta)
+
+func checa_botao_press(delt: float) -> void:
+	## Função que checa se botões foram pressionados e executa as devidas ações
+	
+	# Movimento
+	if Input.is_action_pressed("Cima"):
+		position.y -= velocidade * delt
+	elif Input.is_action_pressed("Baixo"):
+		position.y += velocidade * delt
+	
+	# Seleção das cores
 	if Input.is_action_just_pressed("S"):
 		Global.cor_raquete += 1
 		if Global.cor_raquete > 3:
@@ -25,13 +42,12 @@ func _process(delta: float) -> void:
 		if Global.cor_raquete < 0:
 			Global.cor_raquete = 3
 	
-	$Label.text = str(Global.cor_raquete)
-	
-	if Global.cor_raquete == Global.cor_bola:
-		collision_mask != ~(1 << 1)
-	elif Global.cor_raquete != Global.cor_bola:
-		collision_mask &= ~(1 << 1)
-
-func _physics_process(delta: float) -> void:
-	velocity = velocity.normalized() * velocidade
-	move_and_collide(velocity * delta)
+	# Habilidades especiais
+	if Input.is_action_just_pressed("P"):
+		pass
+	elif Input.is_action_just_pressed("O"):
+		pass
+	elif Input.is_action_just_pressed("N"):
+		pass
+	elif Input.is_action_just_pressed("G"):
+		pass
